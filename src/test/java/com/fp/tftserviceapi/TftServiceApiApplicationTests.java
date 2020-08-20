7f7c7349e7980bca99b8732e20ef0f541e586eff
ForwardPropagation <brian.sanchara@gmail.com>
2020-08-19 20:43:21 -0400

    Spring starter code

---
+++ src/test/java/com/fp/tftserviceapi/TftServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fp.tftserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TftServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
