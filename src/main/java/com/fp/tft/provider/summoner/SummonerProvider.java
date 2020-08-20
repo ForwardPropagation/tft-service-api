cf45fe1abd90e3e93cb4cd65bf37b205138955ae
ForwardPropagation <brian.sanchara@gmail.com>
2020-08-19 23:31:59 -0400

    Updated config

--- src/main/java/com/fp/tft/provider/summoner/SummonerProvider.java
+++ src/main/java/com/fp/tft/provider/summoner/SummonerProvider.java
@@ -3,6 +3,7 @@ package com.fp.tft.provider.summoner;
 import com.fp.tft.riot.api.SummonerV4SummonerDTO;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -10,6 +11,7 @@ import reactor.core.publisher.Mono;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class SummonerProvider {
 
     public static final String SUMMONER_BY_NAME = "by-name/{summonerName}";
@@ -24,6 +26,7 @@ public class SummonerProvider {
                         .path(SUMMONER_BY_NAME)
                         .build(summonerName))
                 .retrieve()
-                .bodyToMono(SummonerV4SummonerDTO.class);
+                .bodyToMono(SummonerV4SummonerDTO.class)
+                .doOnSuccess(response -> log.debug("getSummonerByName Response: {}", response.toString()));
     }
 }
