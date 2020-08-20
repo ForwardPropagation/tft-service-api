dfe37cd7ecf151ff9f5bf48c345fbbff56f4a6e0
ForwardPropagation <brian.sanchara@gmail.com>
2020-08-19 23:51:07 -0400

    Updated tests

--- src/main/java/com/fp/tft/Application.java
+++ src/main/java/com/fp/tft/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {"com.fp.tft"})
-public class TFTServiceApiApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TFTServiceApiApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
--- src/test/java/com/fp/tft/TFTServiceApiApplicationTests.java
+++ src/test/java/com/fp/tft/TFTServiceApiApplicationTests.java
@@ -1,13 +1,22 @@
 package com.fp.tft;
 
+import com.fp.tft.provider.summoner.SummonerProvider;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
 
 @SpringBootTest
+@ActiveProfiles("integration")
 class TFTServiceApiApplicationTests {
+
+	@Autowired
+	private SummonerProvider summonerProvider;
 
 	@Test
 	void contextLoads() {
+		summonerProvider.getSummonerByName("xdeathvdealerx").block();
 	}
 
 }
